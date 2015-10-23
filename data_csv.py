d55aa6b546598a6f673aee6521a56f076976b4f2
raghavjoshi <joshir@seas.upenn.edu>
2015-10-23 00:50:36 -0400

    Now that data is read, calculate cossim

--- data_csv.py
+++ data_csv.py
@@ -3,3 +3,9 @@ import pandas as pd
 # Read in the data
 data = pd.read_csv('data/data.csv',index_col = 0)
 
+#get the cosine similarity scores from data
+def get_cossim_scores(congressman_name):
+	for i in data:
+		if i == congressman_name:
+			return data[i]
+
