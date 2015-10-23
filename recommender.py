211270417f438175f881f05a538ab50c8747e5aa
raghavjoshi <joshir@seas.upenn.edu>
2015-10-23 00:31:03 -0400

    Updated pyscript. Check to see if dropping index column works properly

--- recommender.py
+++ recommender.py
@@ -3,3 +3,8 @@ import scipy.spatial.distance import cosine
 
 #open file
 data = pd.read_csv('data/sen_113_cosp.csv')
+
+# Remove bill column temporarily and check to see if it worked
+temp_data = data.drop('Unnamed: 0',1)
+data.head(6)
+temp_data.head(6)
